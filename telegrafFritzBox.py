34ebf19e69045431d7a8e121a42ab0be61306b97
Schmidsfeld <68500293+Schmidsfeld@users.noreply.github.com>
2020-08-10 11:26:15 +0200

    Update telegrafFritzBox.py
    Some error catching for the cable box users.

--- telegrafFritzBox.py
+++ telegrafFritzBox.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python 
+#!/opt/bin/python3
 
 from fritzconnection import FritzConnection
 import sys
 import os
 
-FRITZBOX_IP = os.environ.get('TGFB_IP', '192.168.178.1')
-FRITZBOX_USER = os.environ.get('TGFB_USER', 'telegraf')
-FRITZBOX_PASSWORD = os.environ.get('TGFB_PASSWD')
-FRITZBOX_ID = os.environ.get('TGFB_ID', 'FrizBox')
+FRITZBOX_IP = os.environ.get('FB_IP', '192.168.178.1')
+FRITZBOX_USER = os.environ.get('FB_USER', 'telegraf')
+FRITZBOX_PASSWORD = os.environ.get('FB_PASSWD' "SuperStrongPassword")
+FRITZBOX_ID = os.environ.get('FB_ID', 'FrizBox')
+
+FRITZBOX_CONNECTION_DSL = True #set to false if you have a cable or IP upstream
 
 try:
     fc = FritzConnection(address=FRITZBOX_IP, user=FRITZBOX_USER, password=FRITZBOX_PASSWORD, timeout=2.0)
@@ -17,41 +19,51 @@ except BaseException:
 
 def readfritz(module, action):
     try:
-        answer= fc.call_action(module, action)
-    except BaseException:
-        print(f"Could not query {module} with action {action}")
-        raise
+        answer = fc.call_action(module, action)
+    except:
+        answer = dict() #return an emty dict in case of failure
     return answer
 
-def extractvar(answer, variable, numeric=False, name=""):
-    avar = str(answer[variable])
-    if name == "":
-        name = variable
-    if numeric:
-        avar = name + '=' + avar +'i'
+def extractvar(answer, variable, integer=False, string=False, name=""):
+    if variable in answer.keys():
+        avar = str(answer[variable])
+        if name == "":
+            name = variable
+        if integer:
+            avar = name + '=' + avar +'i' #format for integers in influxDB
+        else:
+            if string:
+                avar = name + '="' + avar +'"' #format for strings in influxDB
+            else:
+                avar = name + '=' + avar #format for float/double in influxDB
     else:
-        avar = name + '="' + avar +'"'
+        avar = ''
     return avar
 
 def assemblevar(*args):
-    data = ','.join(list(args))
+    data = ','.join(list(args))+','
     #cleaning up output
     data = data.replace("New", "")
+    data = data.replace(",,",",") 
+    data = data.replace(",,",",")
+    data = data[:-1]
     return data
 
 def influxrow(tag, data):
-    influx = FRITZBOX_ID +','+ fbName +  ',source=' + tag + ' ' + data
+    influx = FRITZBOX_ID +','+ fbName +  ',source=' + tag + ' ' + data 
     print(influx)
 
 #Get FritzBox variables
 deviceInfo = readfritz('DeviceInfo1', 'GetInfo')
-#connectionInfo = readfritz('WANIPConn1', 'GetStatusInfo') 
-connectionInfo = readfritz('WANPPPConnection1', 'GetInfo') 
 wanInfo = readfritz('WANCommonIFC1', 'GetCommonLinkProperties')
 trafficInfo = readfritz('WANCommonIFC1', 'GetAddonInfos')
-dslInfo = readfritz('WANDSLInterfaceConfig1', 'GetInfo')
-dslError = readfritz('WANDSLInterfaceConfig1', 'GetStatisticsTotal')
-dslInfo = readfritz('WANDSLInterfaceConfig1', 'GetInfo')
+#connectionInfo = readfritz('WANIPConn1', 'GetStatusInfo') 
+if FRITZBOX_CONNECTION_DSL: #only appliciable if DSL Upstream connection
+    connectionInfo = readfritz('WANPPPConnection1', 'GetInfo') 
+    dslInfo = readfritz('WANDSLInterfaceConfig1', 'GetInfo')
+    dslError = readfritz('WANDSLInterfaceConfig1', 'GetStatisticsTotal')
+    dslInfo = readfritz('WANDSLInterfaceConfig1', 'GetInfo')
+    
 fritzInfo = readfritz('LANHostConfigManagement1', 'GetInfo')
 dhcpInfo = readfritz('Hosts1', 'GetHostNumberOfEntries')
 lanStat = readfritz('LANEthernetInterfaceConfig1', 'GetStatistics')
@@ -74,12 +86,12 @@ fbName = fbName.replace('"','')
 
 upTime = extractvar(deviceInfo, 'NewUpTime', True)
 connectionTime = extractvar(connectionInfo, 'NewUptime', True, 'ConnectionTime')
-connectionStatus = extractvar(connectionInfo, 'NewConnectionStatus', False)
-connectionError = extractvar(connectionInfo, 'NewLastConnectionError', False, 'LastError')
-connectionType = extractvar(wanInfo, 'NewWANAccessType', False)
+connectionStatus = extractvar(connectionInfo, 'NewConnectionStatus', False, True)
+connectionError = extractvar(connectionInfo, 'NewLastConnectionError', False, True, 'LastError')
+connectionType = extractvar(wanInfo, 'NewWANAccessType', False, True)
 
-maxDownRate = extractvar(wanInfo, 'NewLayer1DownstreamMaxBitRate', True,)
-maxUpRate = extractvar(wanInfo, 'NewLayer1UpstreamMaxBitRate', True,)
+maxDownRate = extractvar(wanInfo, 'NewLayer1DownstreamMaxBitRate', True)
+maxUpRate = extractvar(wanInfo, 'NewLayer1UpstreamMaxBitRate', True)
 
 downRate = extractvar(trafficInfo, 'NewByteReceiveRate', True)
 upRate = extractvar(trafficInfo, 'NewByteSendRate', True)
@@ -87,16 +99,15 @@ downPackageRate = extractvar(trafficInfo, 'NewPacketReceiveRate', True)
 upPackageRate = extractvar(trafficInfo, 'NewPacketSendRate', True)
 downTotal = extractvar(trafficInfo, 'NewTotalBytesReceived', True)
 upTotal = extractvar(trafficInfo, 'NewTotalBytesSent', True)
-downTotal64 = extractvar(trafficInfo, 'NewX_AVM_DE_TotalBytesReceived64', True, 'TotalBytesReceived64' )
-downTotal64 = downTotal64.replace('i','')  #Store values as float not integes
-upTotal64 = extractvar(trafficInfo, 'NewX_AVM_DE_TotalBytesSent64', True, 'TotalBytesSent64')
-upTotal64 = upTotal64.replace('i','') #Store values as float not integes
+downTotal64 = extractvar(trafficInfo, 'NewX_AVM_DE_TotalBytesReceived64', False, False, 'TotalBytesReceived64' )
+upTotal64 = extractvar(trafficInfo, 'NewX_AVM_DE_TotalBytesSent64', False, False,'TotalBytesSent64')
 
-externalIP = extractvar(connectionInfo, 'NewExternalIPAddress', False)
-dns = extractvar(connectionInfo, 'NewDNSServers', False)
-localDns = extractvar(fritzInfo, 'NewDNSServers', False,'LocalDNSServer')
-hostsKnown = extractvar(dhcpInfo, 'NewHostNumberOfEntries', True,)
+externalIP = extractvar(connectionInfo, 'NewExternalIPAddress', False, True)
+dns = extractvar(connectionInfo, 'NewDNSServers', False, True)
+localDns = extractvar(fritzInfo, 'NewDNSServers', False, True,'LocalDNSServer')
+hostsKnown = extractvar(dhcpInfo, 'NewHostNumberOfEntries', True, False)
 
+# Try to extract all the DSL Statistics
 dslDown = extractvar(dslInfo, 'NewDownstreamCurrRate', True)
 dslUp = extractvar(dslInfo, 'NewUpstreamCurrRate', True)
 dslMaxDown = extractvar(dslInfo, 'NewDownstreamMaxRate', True)
@@ -116,21 +127,21 @@ hecErrorLocal = extractvar(dslError, 'NewATUCHECErrors', True)
 
 lanPackageUp = extractvar(lanStat, 'NewPacketsSent', True)
 lanPackageDown = extractvar(lanStat, 'NewPacketsReceived', True)
-wlanPackageUp24 = extractvar(wlanStat24, 'NewTotalPacketsSent', True, 'PacketsSent' )
-wlanPackageDown24 = extractvar(wlanStat24, 'NewTotalPacketsReceived', True, 'PacketsReceived')
-wlanPackageUp50 = extractvar(wlanStat50, 'NewTotalPacketsSent', True, 'PacketsSent')
-wlanPackageDown50 = extractvar(wlanStat50, 'NewTotalPacketsReceived', True, 'PacketsReceived')
-wlanPackageUpGuest = extractvar(wlanStatGuest, 'NewTotalPacketsSent', True, 'PacketsSent')
-wlanPackageDownGuest = extractvar(wlanStatGuest, 'NewTotalPacketsReceived', True, 'PacketsReceived')
-wlanName24 = extractvar(wlanInfo24, 'NewSSID', False)
-wlanName50 = extractvar(wlanInfo50, 'NewSSID', False) 
-wlanNameGuest = extractvar(wlanInfoGuest, 'NewSSID', False) 
+wlanPackageUp24 = extractvar(wlanStat24, 'NewTotalPacketsSent', True, False, 'PacketsSent' )
+wlanPackageDown24 = extractvar(wlanStat24, 'NewTotalPacketsReceived', True,  False, 'PacketsReceived')
+wlanPackageUp50 = extractvar(wlanStat50, 'NewTotalPacketsSent', True, False, 'PacketsSent')
+wlanPackageDown50 = extractvar(wlanStat50, 'NewTotalPacketsReceived', True, False, 'PacketsReceived')
+wlanPackageUpGuest = extractvar(wlanStatGuest, 'NewTotalPacketsSent', True, False, 'PacketsSent')
+wlanPackageDownGuest = extractvar(wlanStatGuest, 'NewTotalPacketsReceived',True, False, 'PacketsReceived')
+wlanName24 = extractvar(wlanInfo24, 'NewSSID', False, True)
+wlanName50 = extractvar(wlanInfo50, 'NewSSID', False, True) 
+wlanNameGuest = extractvar(wlanInfoGuest, 'NewSSID', False, True) 
 wlanChannel24 = extractvar(wlanInfo24, 'NewChannel', True)
 wlanChannel50 = extractvar(wlanInfo50, 'NewChannel', True)
 wlanChannelGuest = extractvar(wlanInfoGuest, 'NewChannel', True)
-wlanClients24 = extractvar(wlanAssoc24, 'NewTotalAssociations', True, 'ClientsNumber')
-wlanClients50 = extractvar(wlanAssoc50, 'NewTotalAssociations', True, 'ClientsNumber')
-wlanClientsGuest = extractvar(wlanAssocGuest, 'NewTotalAssociations', True, 'ClientsNumber')
+wlanClients24 = extractvar(wlanAssoc24, 'NewTotalAssociations', True, False, 'ClientsNumber')
+wlanClients50 = extractvar(wlanAssoc50, 'NewTotalAssociations', True, False, 'ClientsNumber')
+wlanClientsGuest = extractvar(wlanAssocGuest, 'NewTotalAssociations', True, False, 'ClientsNumber')
 
 #Output variables as sets of influxdb compatible lines 
 general = assemblevar(model, connectionType, serial, firmware)
@@ -142,8 +153,9 @@ influxrow('status', status)
 wan = assemblevar(connectionTime, maxDownRate, maxUpRate, downRate, upRate,  downPackageRate, upPackageRate, downTotal, upTotal, downTotal64, upTotal64)
 influxrow('wan', wan)
 
-dsl = assemblevar(dslDown, dslUp, dslMaxDown, dslMaxUp, noiseDown, noiseUp, powerDown, powerUp, attenuationDown, attenuationUp, hecError, hecErrorLocal, crcError, crcErrorLocal, fecError, fecErrorLocal )
-influxrow('dsl', dsl)
+if FRITZBOX_CONNECTION_DSL:
+    dsl = assemblevar(dslDown, dslUp, dslMaxDown, dslMaxUp, noiseDown, noiseUp, powerDown, powerUp, attenuationDown, attenuationUp, hecError, hecErrorLocal, crcError, crcErrorLocal, fecError, fecErrorLocal )
+    influxrow('dsl', dsl)
 
 network = assemblevar(externalIP, dns, localDns, hostsKnown)
 influxrow('network', network)
